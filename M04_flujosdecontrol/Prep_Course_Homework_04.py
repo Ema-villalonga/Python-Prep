0e7a0074d8995293acd70f11faaed570cb253ba3
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2024-04-18 14:27:58 -0300

    ....

--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -7,14 +7,23 @@
 
 # In[4]:
 
-
-
-
+valor = -2
+if(valor < 0):
+    print("El numero es MENOR a cero")
+elif(valor > 0):
+    print("El numero es MAYOR a cero")
+else:
+    print("El numero es IGUAL a cero")
 
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
 
 # In[5]:
-
+variable_A = 2
+variable_B = 4
+if(type(variable_A) == type(variable_B)):
+    print("Las variables son del mismo tipo de dato")
+else:
+    print("Las variables son de DIFERENTE tipo de dato")
 
 
 
@@ -22,8 +31,18 @@
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
 
 # In[7]:
+def obtener_numeros(num):
+    pares = [] 
+    impares = []   
+    for i in range(num):
+        if i % 2 == 0: 
+            pares.append(i)
+        elif i % 2 == 1:
+            impares.append(i)    
+    return pares, impares
 
-
+num = 21
+print(obtener_numeros(num))
 
 
 
@@ -31,15 +50,17 @@
 
 # In[9]:
 
-
-
+for i in range(6):
+    print("Rango de valor", i, "Elevado a la tercera potencia", i**3)
+    
 
 
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
 # In[10]:
-
-
+n = 5
+for i in range(n):
+    print(++i)
 
 
 
@@ -47,16 +68,28 @@
 
 # In[33]:
 
-
-
-
+numero = -2
+while True:
+    if numero > 0:
+        print(numero)
+        break
+    else:
+        print("El numero", numero,"no petenece al conjunto de numeros naturales")
+        break
 
 # 7) Crear un ciclo for dentro de un ciclo while
 
 # In[38]:
 
-
-
+txt = "Hola Mundo"
+while True:
+    for palabra in txt:
+        if palabra == "o":
+            print("Esta")
+            continue
+        else:
+            print("No esta")
+    break
 
 
 # 8) Crear un ciclo while dentro de un ciclo for
@@ -71,6 +104,24 @@
 
 # In[54]:
 
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+def generar_primos(limite):
+    primos = []
+    for numero in range(2, limite + 1):
+        if es_primo(numero):
+            primos.append(numero)
+    return primos
+
+limite = 30
+lista_primos = generar_primos(limite)
+print(f"Los primeros números primos son: {lista_primos}")
 
 
 
